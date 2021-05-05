6b2e21bc27edb2c0c6c5af9c36eef64c57344996
Peretyatko Oleg <47569699+Sigeezmund@users.noreply.github.com>
2021-05-05 18:05:36 +0300

    prepare to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+bot: java -jar build/libs/feedly-bot-1.0.jar
--- src/main/kotlin/model/system/SecretSingleton.kt
+++ src/main/kotlin/model/system/SecretSingleton.kt
@@ -1,20 +1,15 @@
 package model.system
 
-import java.io.FileInputStream
-import java.util.Properties
-
 object SecretSingleton {
 
     private var secretProperties: SecretProperties
 
     init {
-        val property = Properties()
-        property.load(FileInputStream("src/main/resources/config.properties"))
         secretProperties = SecretProperties(
-            telegramToken = property.getProperty("telegram.token"),
-            telegramBotName = property.getProperty("telegram.botName"),
-            feedlyAuthToken = property.getProperty("feedly.auth"),
-            feedlyUserId = property.getProperty("feedly.user")
+            telegramToken = System.getenv("telegram.token"),
+            telegramBotName = System.getenv("telegram.botName"),
+            feedlyAuthToken = System.getenv("feedly.auth"),
+            feedlyUserId = System.getenv("feedly.user")
         )
     }
 
